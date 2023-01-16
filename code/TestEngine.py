74b7d92fbfbe1c00d1a143646c6e233a6e131473
Nikhil Mehra <nmehra2@ncsu.edu>
2023-01-15 19:02:37 -0500

    Created test engine

---
+++ code/TestEngine.py
@@ -0,0 +1,13 @@
+import Tests
+from inspect import getmembers, isfunction
+
+tests = [t for t in getmembers(Tests) if isfunction(t[1]) and t[0].startswith("eg_")]
+
+def run():
+    for test in tests:
+        t_name, t_func = test
+        try:
+            t_func()
+            print(f"PASS : {t_name}")
+        except AssertionError:
+            print(f"FAIL : {t_name}")
