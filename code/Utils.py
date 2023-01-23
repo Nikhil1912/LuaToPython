8b0341d3ffbc0bd2aae7dfc99d0276ec4caf32f9
Pathey70 <patheyshah@gmail.com>
2023-01-23 17:48:57 -0500

    Implemented rint method in utils

--- code/Utils.py
+++ code/Utils.py
@@ -8,9 +8,7 @@ def rnd(n, nPlaces=3):
 def rand(lo=0,hi=1):
     Seed = (16807 * Seed) % 2147483647
     return lo + (hi-lo) * Seed / 2147483647
-"""
-function rand(lo,hi) --> n; a float "x" lo<=x < x
-  lo, hi = lo or 0, hi or 1
-  Seed = (16807 * Seed) % 2147483647
-  return lo + (hi-lo) * Seed / 2147483647 end
-"""
+
+def rint(lo,hi):
+    return math.floor(0.5 + rand(lo,hi))
+
