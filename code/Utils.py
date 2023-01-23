b2f6475a4a5b8b339b6de7bbcbf8808d21a10186
Pathey70 <patheyshah@gmail.com>
2023-01-23 17:19:03 -0500

    Implemented rand method

--- code/Utils.py
+++ code/Utils.py
@@ -1,6 +1,16 @@
 import math
-
+Seed=937162211
 
 def rnd(n, nPlaces=3):
     mult = pow(10, nPlaces)
     return math.floor(n * mult + 0.5) / mult
+
+def rand(lo=0,hi=1):
+    Seed = (16807 * Seed) % 2147483647
+    return lo + (hi-lo) * Seed / 2147483647
+"""
+function rand(lo,hi) --> n; a float "x" lo<=x < x
+  lo, hi = lo or 0, hi or 1
+  Seed = (16807 * Seed) % 2147483647
+  return lo + (hi-lo) * Seed / 2147483647 end
+"""
