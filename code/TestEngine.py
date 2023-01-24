bf66aa410fa21d1ae141c16c3232d426786e28f2
pmodi2 <pmodi2@ncsu.edu>
2023-01-23 19:35:10 -0500

    added code for cli parsing and eg_the testcase

--- code/TestEngine.py
+++ code/TestEngine.py
@@ -6,12 +6,12 @@ from inspect import getmembers, isfunction
 tests = [t for t in getmembers(Tests) if isfunction(t[1]) and t[0].startswith("eg_")]
 
 
-def run():
+def run(the):
     fails = 0
     for test in tests:
         t_name, t_func = test
         try:
-            t_func()
+            t_func(the)
             print(f"PASS : {t_name}")
         except AssertionError:
             print(f"FAIL : {t_name}")
--- code/Tests.py
+++ code/Tests.py
@@ -6,20 +6,19 @@ from Num import Num
 from Utils import rnd
 
 
-def eg_check_syms():
+def eg_check_syms(the):
     sym = Sym()
     for x in ["a", "a", "a", "a", "b", "b", "c"]:
         sym.add(x)
     assert 'a' == sym.mid() and 1.379 == rnd(sym.div())
 
 
-def eg_check_nums():
+def eg_check_nums(the):
     num = Num()
     for x in [1, 1, 1, 1, 2, 2, 3]:
         num.add(x)
     assert 11 / 7 == num.mid() and 0.787 == rnd(num.div())
 
-
-if __name__ == "__main__":
-    fails = TestEngine.run()
-    sys.exit(fails)
+def eg_the(the):
+    print(the)
+    pass
